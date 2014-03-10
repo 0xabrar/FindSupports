47883abbd8e353e9c29063f19660818c7889d3b4
RaunaqSuri <raunaq.suri@hotmail.com>
2014-03-09 20:50:22 -0400

    Scratch

---
+++ scratch.php
@@ -0,0 +1,58 @@
+<?php
+    echo "
+    <!DOCTYPE html>
+    <html>
+    <head>
+    ";
+    echo "
+        <title>Find Me A Support League of Legends</title>
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+        <meta charset=\"UTF-8\">
+        <!-- Bootstrap -->  
+        <link href=\"bootstrap/css/bootstrap.css\" rel=\"stylesheet\">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src= \"https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js\"></script>
+          <script src=\"https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js\"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <style> body {background-image:url(\"./images/background.png\"); background-repeat: no-repeat; background-size: cover } </style>
+
+        <div class =\"container\" style=\"margin-top:10%\">
+          <div class=\"center-block\">
+            <!-- The div that holds the title -->
+            <div class=\"center-block\">
+              <a href =\"index.php\"> <img src=\"images/logo.png\" class=\"center-block\"> 
+              </img> </a>
+            </div>
+          </div>
+        </div>
+  
+
+        <div class =\"container\" style=\"margin-top:0%\">
+          <div class=\"center-block\">
+            <!-- The div that holds the title -->
+            <div class=\"center-block\">
+
+                  <table class=\"table table-striped table-bordered table-condensed table-hover\">  
+                    <thead>  
+                      <tr>  
+                        <th>Summoner Name</th>  
+                        <th>Games Played</th>  
+                        <th>Games Won</th>  
+                        <th>Win %</th>  
+                        <th>Total Assists</th>  
+                        <th>Most Played Champion</th>
+                        <th>LolKing Profile</th>
+                        </tr>  
+                    </thead>
+            ";
+    echo "
+    </body>
+    </html>
+    ";
+
+?>
