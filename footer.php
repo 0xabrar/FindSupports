7025ea74c7004efe7f0557e7502c288e43948b6a
RaunaqSuri <raunaq.suri@hotmail.com>
2014-03-16 15:27:54 -0400

    Removed the script src from the footer. You can't have a script listed twice on one doc

--- footer.php
+++ footer.php
@@ -32,10 +32,5 @@
     </div>
 
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-
 </body>
 </html>
